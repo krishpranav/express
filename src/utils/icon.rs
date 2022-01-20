cc20eee1a1f73d9e3841760dd53c5d445830f6ff
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-20 18:57:44 +0530

    utils: icon

--- src/utils/icon.rs
+++ src/utils/icon.rs
@@ -1,6 +1,7 @@
 pub trait Icon {
     fn data(&self) -> String;
-    fn extension(&self) -> String;   
+    fn extension(&self) -> String;
 }
 
-include!(concat(env!("OUT_DIR"), "/icons/enum.rs"));
+include!(concat!(env!("OUT_DIR"), "/icons/enum.rs"));
+include!(concat!(env!("OUT_DIR"), "/icons/impl.rs"));
